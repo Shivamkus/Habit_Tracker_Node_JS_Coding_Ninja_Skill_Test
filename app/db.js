6acec25292f9e44f5aa428b36158096a13ead3ba
shivam kushwah <97247055+Shivamkus@users.noreply.github.com>
2023-10-03 17:45:32 +0530

    Update db.js

--- app/db.js
+++ app/db.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 exports.connectMongoose =()=>{
     mongoose.set("strictQuery", false);
-    mongoose.connect('mongodb+srv://himadrinayak:12345@cluster0.h7n86ah.mongodb.net/csv-upload?retryWrites=true&w=majority', { useNewUrlParser: true })
-    .then((e)=>console.log("Connected to Mongodb => Habit-Tracker"))
+    mongoose.connect('mongodb://0.0.0.0:27017/habitTrackerApp');
+    .then((e)=>console.log("Connected to Mongodb => Habit-Tracker"));
     .catch((e)=>console.log("Not Connect Mongodb", e));
 }
